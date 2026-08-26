94412604eb92e69701e95f58a0fa162e30624494
Yasuto TAKENAKA <y.takenaka@gmail.com>
2026-08-26 10:13:57 +0900

    big change of the repository

--- ch01_03_basics/src/2-cross-valid-max-depth.py
+++ ch01_03_basics/src/2-cross-valid-max-depth.py
@@ -1,15 +1,14 @@
 import matplotlib
-from sklearn import tree
-from sklearn import metrics
 import matplotlib.pyplot as plt
-# import seaborn as sns
 import pandas as pd
+from sklearn import metrics, tree
 
-### the code has already been refactering from its orign code.
+### helper functions
 
-### helper functions.
-def evaluate_train_and_test(model, train_df, test_df, features, target_col="quality"):
-    """return a pair of accuracy score of train-and-test data"""
+def evaluate_train_and_test(
+    model, train_df, test_df, features, target_col="quality"
+):
+    """return a pair of accuracy scores for train and test data"""
     train_acc = metrics.accuracy_score(
         train_df[target_col], model.predict(train_df[features])
     )
@@ -32,13 +31,7 @@ df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 df_train = df.head(1000)
 df_test = df.tail(599)
 
-matplotlib.rc('xtick', labelsize=20)
-matplotlib.rc('ytick', labelsize=20)
-
-train_accuracies = [0.5]
-test_accuracies = [0.5]
-
-results = []
+results_acc = [(0, 0.5, 0.5)]  # (depth, train_accuracy, test_accuracy)
 max_depth = 25
 
 for depth in range(1, max_depth):
@@ -49,10 +42,13 @@ for depth in range(1, max_depth):
         clf, df_train, df_test, cols
     )
 
-    results.append((depth, train_acc, test_acc))
+    results_acc.append((depth, train_acc, test_acc))
 
 ### depict
-depths, train_accs, test_accs = zip(*results)
+depths, train_accs, test_accs = zip(*results_acc)
+
+matplotlib.rc("xtick", labelsize=20)
+matplotlib.rc("ytick", labelsize=20)
 
 plt.figure(figsize=(10, 6))
 plt.plot(depths, train_accs, label="train accuracy")
---
+++ ch05_images/src/ch05_images/__init__.py
@@ -0,0 +1,2 @@
+def hello() -> str:
+    return "Hello from ch05-images!"
